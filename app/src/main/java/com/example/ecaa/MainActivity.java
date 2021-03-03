54b7d6f168a6e617d99db7fb363c8989834acd4c
Arushi_ <arushi-18@goodgod.comm>
2021-03-03 20:33:24 +0530

    Splash screen and Sign Up Page

--- app/src/main/java/com/example/ecaa/MainActivity.java
+++ app/src/main/java/com/example/ecaa/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.ecaa;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button signInBtn,signUpBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInBtn=(Button)findViewById(R.id.sign_in_btn);
+        signUpBtn=(Button)findViewById(R.id.sign_up_btn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
